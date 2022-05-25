08ca2123415b52d63bbb480ea798e29c9346eaf2
Dmitriy Elfimov <elf-dn@yandex.ru>
2022-05-25 14:17:02 +0300

    Converted "weather" to a 'str' type

--- dicts1.py
+++ dicts1.py
@@ -1,4 +1,5 @@
 weather = {"city": "Москва","temperature": "20"}
 print("city:", weather["city"])
 weather["temperature"] = int(weather["temperature"]) - 5
+weather["temperature"] = str(weather["temperature"])
 print("weather:", weather)
